715fd3b273a2bf51e59781762a8cbb00a3010648
senadi2 <senadivihasna@gmail.com>
2026-03-31 09:38:17 +0530

    seed.js updated

--- seed.js
+++ seed.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("./models/user");
 
-mongoose.connect("mongodb://127.0.0.1:27017/nestleDB")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log("MongoDB Error:", err));
+
 
 async function seed() {
 
-    const hashedPassword = await bcrypt.hash("manager123", 10);
+    const hashedPassword = await bcrypt.hash("Manager@123", 10);
 
     await User.create({
         username: "manager@mmnestle.com",
